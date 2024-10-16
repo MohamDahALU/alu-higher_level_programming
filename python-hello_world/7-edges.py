6a3c2f99d173bfb63496ac4d5347c2fe76766926
MohamDahALU <m.dahab@alustudent.com>
2024-10-16 12:35:08 +0200

    task7

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 # why do I need to set them in variables? Still not doing it the "intended way"
 [first, last, middle] = [word[:3], word[-2:], word[1:-1]]
 
-print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[-2:]}")
-print(f"Middle word: {word[1:-1]}")
+print(f"First 3 letters: {first}")
+print(f"Last 2 letters: {last}")
+print(f"Middle word: {middle}")
