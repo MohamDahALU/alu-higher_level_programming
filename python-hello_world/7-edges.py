8f7eb3eb117e3bb79c8213e6102647089215d22f
MohamDahALU <m.dahab@alustudent.com>
2024-10-16 12:37:20 +0200

    task7

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-# why do I need to set them in variables? Still not doing it the "intended way"
+# why does it need to be 8 lines? To give space to use variables. Still not doing it the "intended way", screw you pep8
 [first, last, middle] = [word[:3], word[-2:], word[1:-1]]
 
 print(f"First 3 letters: {first}")
