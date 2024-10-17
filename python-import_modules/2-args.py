0238b7b81d3fb208c827e996cd9a8a1651779ab7
MohamDahALU <m.dahab@alustudent.com>
2024-10-17 12:13:41 +0200

    task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     args = sys.argv
     ln = len(args) - 1
     print("{} {}".format(ln, "argument:" if ln == 1
-                        else "arguments:" if ln > 1 else "arguments."))
+                         else "arguments:" if ln > 1 else "arguments."))
     if ln > 0:
         for i in range(1, len(args)):
             print("{}: {}".format(i, args[i]))
