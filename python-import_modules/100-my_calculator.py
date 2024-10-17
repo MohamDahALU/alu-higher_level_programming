6dc254600b42edb8a9cbf12549a6425187196ef2
MohamDahALU <m.dahab@alustudent.com>
2024-10-17 15:00:54 +0200

    task100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 import sys
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     args = sys.argv
     if len(args) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    if args[2] not in ["+", "-", "*", "/"]:
-        print("Unknown operator. Available operators: +, -, * and /")
+    if args[2] not in ["+", "-", "x", "/"]:
+        print("Unknown operator. Available operators: +, -, x and /")
         exit(1)
 
     a = int(args[1])
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, args[2], b, add(a, b)))
     elif args[2] == "-":
         print("{} {} {} = {}".format(a, args[2], b, sub(a, b)))
-    elif args[2] == "*":
+    elif args[2] == "x":
         print("{} {} {} = {}".format(a, args[2], b, mul(a, b)))
     elif args[2] == "/":
         print("{} {} {} = {}".format(a, args[2], b, div(a, b)))
