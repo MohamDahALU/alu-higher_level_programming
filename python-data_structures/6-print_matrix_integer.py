c21250a4e04b4e34c34465e762bcc06f4efce950
MohamDahALU <m.dahab@alustudent.com>
2024-10-18 07:39:13 +0200

    task6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,10 +4,3 @@ def print_matrix_integer(matrix=[[]]):
         for col in row:
             print("{:d}".format(col), end=" ")
         print("")
-
-
-print_matrix_integer([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-])
