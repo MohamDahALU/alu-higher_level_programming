9c09dba182b966f0ea8873914bebe2a4598b98e0
MohamDahALU <m.dahab@alustudent.com>
2024-10-31 08:41:06 +0200

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -47,7 +47,7 @@ class Square:
         """
         Sets the position of the square.
         """
-        if value != (int, int) and (value[0], value[1]) < (0, 0):
+        if value != (int, int) or (value[0], value[1]) < (0, 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             return value
