2b3830e1ad7146debd720f38a3ed0822b5917651
MohamDahALU <m.dahab@alustudent.com>
2024-11-03 09:32:54 +0200

    task10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -64,9 +64,3 @@ class Square(BaseGeometry):
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-
-s = Square(13)
-
-print(s)
-print(s.area())
