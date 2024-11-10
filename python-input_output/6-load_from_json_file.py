315d85181a626454e6bc8479bb6fa62e147003cf
MohamDahALU <m.dahab@alustudent.com>
2024-11-10 10:51:00 +0200

    task6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module provides a function to load data from a JSON file.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Load data from a JSON file and return it as a Python object.
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.loads(f)
