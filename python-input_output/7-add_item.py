3c9768e2c3cad0a1d5481c0702bfda9e8a611bb1
MohamDahALU <m.dahab@alustudent.com>
2024-11-10 11:37:02 +0200

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,9 +4,10 @@ This module provides a script to add command line
 arguments to a list stored in a JSON file.
 """
 import sys
+import importlib
 
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = importlib.import_module("6-load_from_json_file").load_from_json_file
+save_to_json_file = importlib.import_module("5-save_to_json_file").save_to_json_file
 
 
 def add_item():
