b7c5bdf936cbacc7578d65e619aea83a88fbc195
MohamDahALU <m.dahab@alustudent.com>
2024-11-10 10:52:46 +0200

    task6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -11,4 +11,5 @@ def load_from_json_file(filename):
     """
 
     with open(filename, "r", encoding="utf-8") as f:
-        return json.loads(f)
+        jsonObj = f.read()
+        return json.loads(jsonObj)
