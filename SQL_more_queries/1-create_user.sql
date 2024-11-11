bae71422f119aef9ba2d76ec00109981db0995cf
MohamDahALU <m.dahab@alustudent.com>
2024-11-11 08:51:18 +0200

    task1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Creates new user
+-- Creates users and grants them all privilages
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
