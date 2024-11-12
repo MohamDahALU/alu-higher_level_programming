a5fe91b8d70b765498e2360f677ef68b8fcd95b5
MohamDahALU <m.dahab@alustudent.com>
2024-11-12 08:38:13 +0200

    task8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- List cities in the state of california
-SELECT * FROM cities WHERE state_id IN (
+SELECT id, name FROM cities WHERE state_id IN (
     SELECT id FROM states WHERE name = 'California'
 );
