9859aa5df752c650449180539b638f4df01e29ae
MohamDahALU <m.dahab@alustudent.com>
2024-11-17 16:40:58 +0200

    task1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""This script displays the value"""
-import urllib.request
+"""
+This script takes a URL as a command-line argument, sends a request to the URL,
+and displays the value of the 'X-Request-Id' variable found in the header of the response.
+"""
 import sys
 
+import urllib.request
 
-with urllib.request.urlopen(sys.argv[1]) as request:
-    print(request.getheader("X-Request-Id"))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        header = response.info()
+        print(header.get('X-Request-Id'))
