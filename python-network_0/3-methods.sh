8bf6658d1cce053a33a2d9c7d15128d388538e45
MohamDahALU <m.dahab@alustudent.com>
2024-11-17 14:11:52 +0200

    task3

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # print all allowed methods
-curl -i -sL -X OPTIONS "0.0.0.0:5000/route_4" | grep "Allow" | cut -f2 -d":" | tail -c+2
+curl -i -sL -X OPTIONS "$1" | grep "Allow" | cut -f2 -d":" | tail -c +2
+
+
