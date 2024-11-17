cf3dea873dc23c963610d331452fc77c778f1081
MohamDahALU <m.dahab@alustudent.com>
2024-11-17 16:56:16 +0200

    task2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-This is a script that makes a post request with data and
-then prints the response.
+This script makes a post request with an email
 """
-
 import sys
+
 import urllib.request
+import urllib.parse
 
-
-data = urllib.parse.urlencode({"email": sys.argv[2]}).encode("utf-8")
-
-req = urllib.request.Request(sys.argv[1], data)
-
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode("utf-8")
-    print(body)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    
+    req = urllib.request.Request(url, data)
+    
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
