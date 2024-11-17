08deeb86b2f0129734ea389fe12a7f5d7530eba7
MohamDahALU <m.dahab@alustudent.com>
2024-11-17 16:57:39 +0200

    task2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -10,11 +10,11 @@ import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    
+
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+
     req = urllib.request.Request(url, data)
-    
+
     with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
+        body = response.read().decode("utf-8")
         print(body)
