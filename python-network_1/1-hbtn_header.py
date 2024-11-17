d39d391f48d9899c361c06089bfb2094c2ec77e1
MohamDahALU <m.dahab@alustudent.com>
2024-11-17 16:34:13 +0200

    task1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
-import sys
-import urllib.request
-
 """
 This script displays the value of the 'X-Request-Id' header found in the response.
 """
+import urllib.request
+import sys
+
 
 with urllib.request.urlopen(sys.argv[1]) as request:
     print(request.getheader("X-Request-Id"))
