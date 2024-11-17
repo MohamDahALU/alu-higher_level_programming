633eaf4063ea795c9c7e4e180a411ef8d6c74e09
MohamDahALU <m.dahab@alustudent.com>
2024-11-17 16:47:44 +0200

    task1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -8,5 +8,4 @@ import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
-        data = response.read()
-        print(data.getheader("X-Request-Id"))
+        print(response.getheader("X-Request-Id"))
