93ee59430faae90fd94e0748899450ef6e6ec35d
MohamDahALU <m.dahab@alustudent.com>
2024-11-18 10:36:27 +0200

    task4

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This script makes a displays a response using the requests package.
+"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alu-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
