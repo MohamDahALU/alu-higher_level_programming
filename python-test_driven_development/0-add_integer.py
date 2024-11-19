0336c873970c880c92da012898d8e95af9b3937f
MohamDahALU <m.dahab@alustudent.com>
2024-11-19 09:56:23 +0200

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,14 +9,11 @@ def add_integer(a, b=98):
     Adds two integers.
     """
 
-    try:
-        float(a)
-    except Exception:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    try:
-        float(b)
-    except Exception:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+        
 
     a = int(a)
     b = int(b)
