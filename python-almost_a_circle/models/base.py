8889fdeb576d057c42cd263b71a4a0d8aeefdefd
MohamDahALU <m.dahab@alustudent.com>
2024-11-21 08:54:52 +0200

    task1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
+
 class Base:
+    """
+    Base class for managing id attribute in all future classes and to avoid duplicating the same code.
+    """
+
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
+        """
+        Initialize a new Base instance.
+        """
+
         if id != None:
             self.id = id
         else:
