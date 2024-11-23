16fc4b08afb1fbf19d2fe12ee5846b877c7fc949
MohamDahALU <m.dahab@alustudent.com>
2024-11-23 10:06:12 +0200

    task2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,26 @@
 """
 This module defines the Rectangle class, which inherits from the Base class.
 """
-Base = __import__("base").Base
+
+
+class Base:
+    """
+    Base class for managing id attribute in all future
+    classes and to avoid duplicating the same code.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new Base instance.
+        """
+
+        Base.__nb_objects += 1
+        if id is not None:
+            self.id = id
+        else:
+            self.id = self.__nb_objects
 
 
 class Rectangle(Base):
