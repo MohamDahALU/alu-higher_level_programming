ddff27a6daa6f75f1ae8baa49484408e34cb5330
MohamDahALU <m.dahab@alustudent.com>
2024-11-23 13:39:59 +0200

    task14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -59,3 +59,14 @@ class Square(Rectangle):
             self.x = args[2]
         if len(args) >= 4:
             self.y = args[3]
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of the Square instance.
+        """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
