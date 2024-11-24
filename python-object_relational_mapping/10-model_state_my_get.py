8858535aafbb98f2cb02ca449b757a36a2b8dc3d
MohamDahALU <m.dahab@alustudent.com>
2024-11-24 15:44:01 +0200

    task10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database
+that contain the letter 'a' in their name, ordered
+by state id.
+"""
+
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbName = sys.argv[3]
+    search = sys.argv[4]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username,
+            password,
+            dbName
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+
+    session = Session()
+    states = session.query(State).where(
+        State.name == search
+    ).order_by(State.id).all()
+
+    if states:
+        for state in states:
+            print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    session.close()
+
+    session.close()
