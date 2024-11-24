707accee4209b5369d92fe2e875ed97320bfee44
MohamDahALU <m.dahab@alustudent.com>
2024-11-24 08:54:23 +0200

    test_square

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -40,22 +40,22 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(str(err.exception), "y must be an integer")
     
     def test_square_one_arg_negative(self):
-        with self.assertRaises(TypeError) as err:
+        with self.assertRaises(ValueError) as err:
             Square(0)
         self.assertEqual(str(err.exception), "width must be > 0")
         
     def test_square_one_arg_negative(self):
-        with self.assertRaises(TypeError) as err:
+        with self.assertRaises(ValueError) as err:
             Square(-1)
         self.assertEqual(str(err.exception), "width must be > 0")
     
     def test_square_two_arg_negative(self):
-        with self.assertRaises(TypeError) as err:
+        with self.assertRaises(ValueError) as err:
             Square(1, -2)
         self.assertEqual(str(err.exception), "x must be >= 0")
     
     def test_square_three_arg_negative(self):
-        with self.assertRaises(TypeError) as err:
+        with self.assertRaises(ValueError) as err:
             Square(1, 2, -3)
         self.assertEqual(str(err.exception), "y must be >= 0")
 
