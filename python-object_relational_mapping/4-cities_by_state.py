f616fee8a913794b7605c3adc2d6bf42cdf1df9f
MohamDahALU <m.dahab@alustudent.com>
2024-11-24 13:01:38 +0200

    task4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     )
     cur = conn.cursor()
     cur.execute(
-        "SELECT cities.id, states.name, cities.name FROM cities\
+        "SELECT cities.id, cities.name, states.name FROM cities\
          JOIN states ON cities.state_id = states.id"
     )
     query_rows = cur.fetchall()
