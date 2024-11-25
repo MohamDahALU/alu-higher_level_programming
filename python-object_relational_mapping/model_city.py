1ceda9d6e5e090ed82e8ddd339b01b5b785eab55
MohamDahALU <m.dahab@alustudent.com>
2024-11-25 07:51:37 +0200

    task14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines the City class to interact with the cities table in the database.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+
+class City(Base):
+
+    __tablename__ = "cities"
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True,
+        unique=True
+    )
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
