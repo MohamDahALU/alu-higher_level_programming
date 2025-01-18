8f1c4de7a182b4b3c75e1b8457f429eb50d09561
MohamDahALU <m.dahab@alustudent.com>
2025-01-18 15:24:59 +0200

    commit for 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,11 +4,11 @@ const arg = +process.argv[2];
 if (isNaN(arg)) {
   console.log(1);
 } else {
-  console.log(fact(arg, arg));
+  console.log(fact(arg));
 }
 
-function fact (a, n) {
-  if (n === 1) return a;
+function fact (n) {
+  if (n === 1) return n;
 
-  return fact(a * (n - 1), n - 1);
+  return n * fact(n - 1);
 }
