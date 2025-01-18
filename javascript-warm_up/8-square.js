d9e3d87b8696cab258626d93a966ec04c882d9b0
MohamDahALU <m.dahab@alustudent.com>
2025-01-18 15:10:09 +0200

    commit for 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const arg = +process.argv[2];
 
 if (isNaN(arg)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < arg; i++) {
     console.log('X'.repeat(arg));
