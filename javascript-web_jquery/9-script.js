cb0c7a676a224a88c10da3cbd7daa994108cc909
MohamDahALU <m.dahab@alustudent.com>
2025-02-02 13:04:31 +0200

    commit for 9-script.js

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
+  // The old URL doesn't work.
   $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', (data) => {
     $('#hello').text(data.hello);
   });
